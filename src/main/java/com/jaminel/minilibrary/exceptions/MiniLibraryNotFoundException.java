da7bfb2471d1d296a81a3910d7513348378e70f8
jaminel <jamineltechnology@gmail.com>
2023-12-17 01:42:48 +0100

    Mini Library

---
+++ src/main/java/com/jaminel/minilibrary/exceptions/MiniLibraryNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jaminel.minilibrary.exceptions;
+
+public class MiniLibraryNotFoundException extends RuntimeException{
+    public MiniLibraryNotFoundException(String message) {
+        super(message);
+    }
+
+    public MiniLibraryNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
